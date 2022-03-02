c0df05f5e259b803fc2762a468beffd8512f95f9
wmboon <wietseboon@gmail.com>
2022-03-02 14:48:11 +0100

    included 3d case with three intersecting fractures

--- examples/case3/main.py
+++ examples/case3/main.py
@@ -7,18 +7,16 @@ import pygeon as pg
 
 def main():
 
-
     # The fractures are specified by their vertices, stored in a numpy array
     f_1 = pp.Fracture(np.array([[0, 0, 0, 0], [1, 0, -1, 0], [0, 1, 0, -1]]))
     f_2 = pp.Fracture(np.array([[-1, 0, 1, 0], [0, 0, 0, 0], [0, 1, 0, -1]]))
     f_3 = pp.Fracture(np.array([[1, 0, -1, 0], [0, 1, 0, -1], [0, 0, 0, 0]]))
 
     # Also define the domain
-    domain = {'xmin': -1.1, 'xmax': 1.1, 'ymin': -1.1, 'ymax': 1.1, 'zmin': -1.1, 'zmax': 1.1}
+    domain = {'xmin': -2, 'xmax': 2, 'ymin': -2, 'ymax': 2, 'zmin': -2, 'zmax': 2}
 
     # Define a 3d FractureNetwork, similar to the 2d one
-    # network = pp.FractureNetwork3d([f_1, f_2, f_3], domain=domain)
-    network = pp.FractureNetwork3d([f_1, f_2], domain=domain)
+    network = pp.FractureNetwork3d([f_1, f_2, f_3], domain=domain)
     mesh_args = {'mesh_size_frac': 0.3, 'mesh_size_min': 0.2}
 
     # Generate the mixed-dimensional mesh
