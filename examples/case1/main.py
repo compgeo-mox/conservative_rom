9c9ccd88bb77fb1a3257e9a23e752c5da2bb692c
wmboon <wietseboon@gmail.com>
2022-03-04 17:46:29 +0100

    introduced one-grid gridbucket

--- examples/case1/main.py
+++ examples/case1/main.py
@@ -33,7 +33,7 @@ def reference_solution(data_key, g, data, discr):
     return q, p
 
 def main(N=2):
-
+    data_key = 'flow'
     # 2D
     g = pp.StructuredTriangleGrid([N]*2, [1]*2)
     # g = pp.CartGrid([N]*2, [1]*2)
@@ -42,15 +42,17 @@ def main(N=2):
     # g = pp.StructuredTetrahedralGrid([N]*3, [1]*3)
     # g = pp.CartGrid([N]*3, [1]*3)
 
+    # Set up grid bucket consisting of one grid
     g.compute_geometry()
-    pg.compute_edges(g)
+    gb = pp.meshing.grid_list_to_grid_bucket([[g]])
+    setup_data(gb)
 
-    data, data_key = setup_data(g)
+    pg.compute_edges(gb)
 
     discr = pp.RT0(data_key)
     # discr = pp.MVEM(data_key)
 
-    hs = HodgeSolver(g, discr, data, data_key)
+    hs = HodgeSolver(gb, discr)
 
     # def linalg_solve(A, b):
     #     amg = pyamg.ruge_stuben_solver(A.tocsr())
@@ -60,6 +62,7 @@ def main(N=2):
     q, p = hs.solve()
 
     # verification
+    data = gb.node_props(g)
     q_ref, p_ref = reference_solution(data_key, g, data, discr)
 
     f = data[pp.PARAMETERS][data_key]['source']
@@ -69,21 +72,20 @@ def main(N=2):
     print("Mass loss:      {:.2E}".format(np.linalg.norm(hs.div*q - f)))
 
 
-def setup_data(g):
-    # Set up discretization
-    perm = pp.SecondOrderTensor(
-        kxx=4*np.ones(g.num_cells), kyy=np.ones(g.num_cells), kxy=np.ones(g.num_cells))
-    b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
-    bc = pp.BoundaryCondition(g, b_faces, ["dir"]*b_faces.size)
-    bc_val = np.zeros(g.num_faces)
-    f = g.cell_volumes
+def setup_data(gb):
+    for g, d in gb:
+        # Set up parameters
+        perm = pp.SecondOrderTensor(
+            kxx=4*np.ones(g.num_cells), kyy=np.ones(g.num_cells), kxy=np.ones(g.num_cells))
+        b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
+        bc = pp.BoundaryCondition(g, b_faces, ["dir"]*b_faces.size)
+        bc_val = np.zeros(g.num_faces)
+        f = g.cell_volumes
 
-    parameters = {"second_order_tensor": perm,
-                  "bc": bc, "bc_values": bc_val, "source": f}
-    data_key = "flow"
-    data = pp.initialize_default_data(g, {}, data_key, parameters)
-
-    return data, data_key
+        parameters = {"second_order_tensor": perm,
+                    "bc": bc, "bc_values": bc_val, "source": f}
+        data_key = "flow"
+        pp.initialize_default_data(g, d, data_key, parameters)
 
 
 if __name__ == "__main__":
