- changed setup of case 2 - fixed flux error computation

examples/case2/setup.py
@@ -3,17 +3,17 @@ import porepy as pp
 
 def gb():
 
-    p = np.array([[0., 1.], 
-                  [0., 0.]])
+    p = np.array([[0.0, 0.9], 
+                  [0.5, 0.5]])
     e = np.array([[0], [1]])
 
-    domain = {"xmin": 0, "xmax": 1, "ymin": -1, "ymax": 1}
+    domain = {"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1}
     network = pp.FractureNetwork2d(p, e, domain)
 
     # set the mesh size
     mesh_size = 1
     mesh_kwargs = {"mesh_size_frac": mesh_size,
-                   "mesh_size_min": mesh_size}
+                   "mesh_size_min": mesh_size / 20}
 
     # create the grid bucket
     return network.mesh(mesh_kwargs)

src/reference.py
@@ -69,5 +69,5 @@ def dim_check(q, p, q_ref, p_ref, hs):
 
     # import pdb; pdb.set_trace()
     print("Pressure error: {:.2E}".format(np.linalg.norm(p-p_ref)))
-    print("Flux error:     {:.2E}".format(np.linalg.norm(q-q_ref)))
+    print("Flux error:     {:.2E}".format(np.sqrt(np.dot(q-q_ref, hs.mass * (q-q_ref)))))
     print("Mass loss:      {:.2E}".format(np.linalg.norm(hs.div*q - f)))